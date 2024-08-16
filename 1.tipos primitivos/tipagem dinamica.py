3432f14457fb2db8fefed1d6acce7e4811e362a6
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-08-16 18:14:13 +0000

    comandos iniciais.

---
+++ 1.tipos primitivos/tipagem dinamica.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("cls || clear") #limpa o terminal
+
+
+
+nome = "marta"
+idade = 22
+altura = 1.65
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
+
---
+++ 1.tipos primitivos/tipagem estatica.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("cls || clear") #limpa o terminal
+
+
+#tipagem estática.
+nome : str # cadeia
+idade : int # inteiro
+altura : float #real
+
+nome = "marta"
+idade = 20
+altura = 1.65
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
