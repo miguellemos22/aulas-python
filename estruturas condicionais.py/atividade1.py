1dcb234ed42dc8bf7bb2f3ce5e44d412df4aad4f
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-08-20 19:53:25 +0000

    AULAS PYTHON

--- estruturas condicionais.py/atividade1.py
+++ estruturas condicionais.py/atividade1.py
@@ -1,2 +1,22 @@
 import os 
 os.system("cls || clear ")
+
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+nota1 = float(input("digite sua primeira nota: "))         
+nota2 = float(input("digite sua segunda nota: "))
+nota3 = float(input("digite sua terceira nota: "))
+media = (nota1 + nota2 + nota3) /3
+
+if media < 7:
+    print("reprovado")
+
+else:
+    print("aprovado")
+
+print(f"nome do aluno: {nome}")
+print(f"idade do aluno: {idade}")
+print(f"primeira nota: {nota1}")
+print(f"segunda nota: {nota2}")
+print(f"terceira nota: {nota3}")
+print(f"media do aluno: {media}")
