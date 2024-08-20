8211b397f1d19ae3ade898755dc102578b66801c
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-08-20 19:34:14 +0000

    aulas-python

---
+++ 1.tipos primitivos/solicitando.py
@@ -0,0 +1,16 @@
+import os 
+os.system("cls || clear ") #limpa o terminal
+
+#solicitando dados
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+altura = float(input("digite sua altura: "))
+
+
+#exibindo dados
+#print(f"meu nome é: {nome}")
+#print(f"idade: {idade} anos.")
+#print(f"altura: {altura} de altura.")
+print(f"nome: {nome}, idade: {idade} anos e {altura} de altura")
+#pode ser feito junto
+print("Bem vindo!!!")
---
+++ estruturas condicionais.py/exemplo-idade.py
@@ -0,0 +1,16 @@
+import os 
+os.system("cls || clear ")
+
+#solicitando dados
+idade = int(input("digite sua idade: "))
+#verificando
+# true é igual a verdadeiro
+#false é igual a falso
+if idade < 18:
+    print("menor de idade.")
+else:
+    print("maior de idade.")    
+    
+    
+
+print("    FIM   ")
