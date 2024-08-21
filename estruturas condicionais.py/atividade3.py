1cf1ddd2482dc49b8699432a46fc5c299a7fd9f1
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-08-21 17:19:42 +0000

    aulas python

---
+++ estruturas condicionais.py/atividade3.py
@@ -0,0 +1,12 @@
+import os 
+os.system("cls || clear ")
+
+login = input("digite seu login: ")
+senha = int(input("digite sua senha: "))
+
+if login != "migs" or senha != 123:
+
+    print("login ou senha incorretos")
+else:
+    print("bem vindo!!") 
+       
