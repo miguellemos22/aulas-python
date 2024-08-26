f59717613235c0735333b743865d4d5fa4c7fb74
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-08-26 17:35:27 +0000

    aulas python

--- laço de repetiçaõ.py/atividade4.py
+++ laço de repetiçaõ.py/atividade4.py
@@ -1,13 +1,14 @@
 import os 
 import time
 os.system("cls || clear ")
+
 soma = 0
 media = 0
 
 for i in range(4):
     notas = float(input(f"digite {i + 1}º nota: "))
-
+    
 soma = soma + notas
-media = media + notas
+media = media + notas / 4
 
 print(f"media: {media}")
