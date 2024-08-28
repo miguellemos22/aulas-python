48187eac59693d88d57c22b3540045165c69b127
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-08-28 19:03:05 +0000

    aulas python

---
+++ laço de repetiçaõ.py/atividade 9.py
@@ -0,0 +1,21 @@
+import os 
+import time
+os.system("cls || clear ")
+
+
+
+
+
+while True:
+    login = input("digite seu login: ")
+    senha =int(input("digite sua senha: "))
+
+    if login == "migs" and senha == 123:
+            print("bem vindo") 
+            break
+    else: 
+            print("tente novamente")
+            
+        
+     
+        
