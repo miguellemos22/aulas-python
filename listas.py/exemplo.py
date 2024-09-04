86c2472635ccfd5ff1f1538d74cf526cb5632091
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-09-04 19:06:34 +0000

    aulas python

---
+++ listas.py/exemplo.py
@@ -0,0 +1,23 @@
+import os 
+os.system("cls || clear ")
+
+# funções sem retorno.
+
+def logo_senai():
+    os.system("cls || clear ")
+    print("""================
+      SENAI
+================""")
+
+logo_senai()
+nome = input("digite seu nome: ")
+
+
+logo_senai()
+idade = input("digite sua idade: ")
+
+logo_senai()
+nota = input("digite uam nota: ")
+
+
+
