ec2513c7320b5768e264f5199076071464bbe9f3
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-09-13 19:08:45 +0000

    aulas python

---
+++ vetores.py/atividade 8.py
@@ -0,0 +1,11 @@
+import os 
+os.system("cls || clear ")
+
+lista_de_numeros = [1, 2, 3]
+
+print("exibindo numeros: ")
+for numero in lista_de_numeros:
+    print(numero)
+print("\nexibindo numeros em ordem inversa: ")
+for numero in reversed(lista_de_numeros):
+    print(numero)
